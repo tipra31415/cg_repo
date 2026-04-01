4215c4d0cd4ffe2f0f625a302fa4b5ca6cf9d318
Arpit Bisht <arpitbisht.31415@gmail.com>
2026-04-01 14:10:23 +0530

    made changes

--- Test/src/main/java/com/cg/TestApplication.java
+++ Test/src/main/java/com/cg/TestApplication.java
@@ -8,6 +8,6 @@ public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
-		System.out.println("Line 1");
+		System.out.println("Arpit");
 }
 }
