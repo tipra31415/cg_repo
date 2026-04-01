9bbd0290511985ba0cd1991b9bcd5d6db82ebb54
Arpit Bisht <arpitbisht.31415@gmail.com>
2026-04-01 12:56:13 +0530

    Conflict resolved

---
+++ Test/src/main/java/Customer.java
@@ -0,0 +1,27 @@
+
+public class Customer {
+	private String name;
+	private int age;
+	private long id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	
+	
+}
--- Test/src/main/java/com/cg/TestApplication.java
+++ Test/src/main/java/com/cg/TestApplication.java
@@ -9,10 +9,5 @@ public class TestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 		System.out.println("Line 1");
-<<<<<<< HEAD
-=======
-		
->>>>>>> 43e8f7079374dda42828cbd81d1c6cf145c644fd
-	}
-
+}
 }
